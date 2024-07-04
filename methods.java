3973e3388d10d943d2cb7aa754346833a7ee1ed3
Ankur4600 <tinkushrivastav78@gmail.com>
2024-07-04 23:06:57 +0530

    function/ method

--- methods.java
+++ methods.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 //method in java
 public class methods {
     public static void main(String[] args) {
-        // add();
+       
         int x=100;
         int y=200;
         int z=500;
-       int a= add(x,y,z);
+       int a= add(x,y,z);//function calling
        System.out.println(a);
+       add();//function calling
+       multiply(x,y,z);//calling another function for multiplication
         
     }
-    //function without passing any parameter
+ //function without passing any parameter
     static void add(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the first number and second number and third number");
@@ -22,8 +24,14 @@ public class methods {
         int c=a+b;
         System.out.println("the sum of the number is "+c);
     }
+ //parameterise function
     static int add (int a,int b,int c){
        
         return a+b+c;
     }
+ //function for multiply
+    static float multiply(float a,float b,float c){
+        return a*b*c;
+    }
+
 }
